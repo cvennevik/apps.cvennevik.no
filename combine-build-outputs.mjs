ea7666e607c0515704d5b36aec0dfdd9d389726a
cvennevik <cvennevik@gmail.com>
2023-06-25 20:11:31 +0200

    Initial commit: root page, hello-world, build script

---
+++ combine-build-outputs.mjs
@@ -0,0 +1,13 @@
+import { cp, rm } from "fs/promises";
+
+// Remove existing files
+await rm("dist", { recursive: true, force: true });
+
+// Copy root page to dist
+await cp("root", "dist", { recursive: true });
+
+// Copy each web app to its own sub-directory
+const appNames = ["hello-world"];
+for (const appName of appNames) {
+  await cp(`${appName}/dist`, `dist/${appName}`, { recursive: true });
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+# apps.cvennevik.no
